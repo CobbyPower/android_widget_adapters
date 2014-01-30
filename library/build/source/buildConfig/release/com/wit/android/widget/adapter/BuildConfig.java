922cfd176dbf188d492c72478cd15fd198fb0d11
GrayWolf <gray.wolf@wolf-itechnologies.com>
2014-01-30 10:26:53 +0100

    updated

---
+++ library/build/source/buildConfig/release/com/wit/android/widget/adapter/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wit.android.widget.adapter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.wit.android.widget.adapter";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 110;
+}
